4450905d76c7adac643f7df8756ab76e5f96e577
Mujahid Hossain <opee.home@outlook.com>
2024-05-20 18:15:19 +0600

    Create Basic.cpp

---
+++ Basic.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2;
+    char operation;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter an operator (+, -, *, /): ";
+    cin >> operation;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    double result;
+    switch (operation) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                cout << "Error: Division by zero!" << endl;
+                return 1;
+            }
+            break;
+        default:
+            cout << "Invalid operator!" << endl;
+            return 1;
+    }
+
+    cout << "Result: " << result << endl;
+    return 0;
+}
